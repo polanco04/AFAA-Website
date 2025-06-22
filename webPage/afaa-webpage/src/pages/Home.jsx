fb882b72003be1e07429cfbbebf78295b80077d1
polanco04 <capgbm@gmail.com>
2025-06-21 22:37:12 -0600

    What I have so far

---
+++ webPage/afaa-webpage/src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import '../css/Home.css'
+
+function Home(){
+    return ( 
+    <>
+        <div id="TitleBox">
+            <h1 id="homeTitle">A Fleet Among Angels</h1>
+        </div>
+        <div id="HomeText">
+            <p id="subtitle">A new tactical SRPG forging its path among the stars!</p>
+            <p id="bodyContent">Welcome to the official Web Page fo A Fleet Among Angels, an exciting new browser-based <br></br>
+            Tactical Strategy Role Playing Game (SRPG) currently in active development!</p>
+        </div>
+    </>    
+    );
+}
+
+export default Home;
