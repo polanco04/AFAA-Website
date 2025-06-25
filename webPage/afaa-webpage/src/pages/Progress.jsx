e6fe32b41d627d36596af43e19b587b93781cfb3
polanco04 <capgbm@gmail.com>
2025-06-25 17:07:07 -0600

    worked on button, sends to progress page for the moment

--- webPage/afaa-webpage/src/pages/Progress.jsx
+++ webPage/afaa-webpage/src/pages/Progress.jsx
@@ -6,13 +6,15 @@ function Progress(){
         <div className="progressBox">
         <h1 className="progressTitle">Progress</h1>
         </div>
-        <p className="progressBody">“A Fleet Among Angels” is actively under development. The game is still in the early stages of development. 
-        <br/><br/><br/> While there is no current progress to show, here is what we are working on:
-		<br/><br/>- Establishing core gameplay mechanics (Movement, attacks, abilities)
-		<br/>- Designing and implementing the initial set of units and their skills
-		<br/>- Developing the first playable levels and enemy AI to publish our demo.
-		<br/>- Crafting the art style and user interface.
+        <div className="body">
+            <p className="progressBody">“A Fleet Among Angels” is actively under development. The game is still in the early stages of development. 
+            <br/><br/><br/> While there is no current progress to show, here is what we are working on:
+		    <br/><br/>- Establishing core gameplay mechanics (Movement, attacks, abilities)
+            <br/>- Designing and implementing the initial set of units and their skills
+            <br/>- Developing the first playable levels and enemy AI to publish our demo.
+            <br/>- Crafting the art style and user interface.
         </p>
+        </div>
     </div>
     );
 }
