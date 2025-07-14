4f7d9fa582e5988a640e26e5173658f990697695
ZachBarrentine <zach.barrentine27@gmail.com>
2025-07-14 14:24:13 -0500

    Update navbar hover, About page, Home title box font gradient

--- webPage/afaa-webpage/src/pages/AboutTheProject.jsx
+++ webPage/afaa-webpage/src/pages/AboutTheProject.jsx
@@ -7,12 +7,12 @@ function AboutTheProject(){
             <h1 className="projectTitle">About the Project</h1>
         </div><div>
                 <h1 className ="projectHeadline">About Us</h1>
-                <p className="AboutTheProjectBody">
-                - We are a development team composed of <span className="highlightGreen">six undergraduate members</span>.
-                <br/><br/>- The purpose of this project is to grow our proficiency in our own respective roles through a digital product that anyone can experience.
-                <br/><br/>- Our goal is to create a unique and engaging gaming experience that combines tactical strategy with an immersive narrative.
-                <br/><br/>- Of the six members, four are pursuing <span className="highlightGreen">computer science</span>, one is pursuing <span className="highlightGreen">graphic design</span>, and one is pursuing <span className="highlightGreen">marketing</span>.
-                </p>
+                <ul className="AboutTheProjectBody" listSpacing>
+                <li>We are a development team composed of <span className="highlightGreen">six undergraduate members</span>.</li>
+                <li>The purpose of this project is to grow our proficiency in our own respective roles through a digital product that anyone can experience.</li>
+                <li>Our goal is to create a unique and engaging gaming experience that combines tactical strategy with an immersive narrative.</li>
+                <li>Of the six members, four are pursuing <span className="highlightGreen">computer science</span>, one is pursuing <span className="highlightGreen">graphic design</span>, and one is pursuing <span className="highlightGreen">marketing</span>.</li>
+                </ul>
                 <h1 className="projectHeadline">About the Game</h1>
                 <p className="AboutTheProjectBody indented">
                 <span className="highlightRed">A Fleet Among Angels</span> is a project that wears its inspirations on its sleeves; Franchises such as <span className="highlightBlue">Legend of the Galactic Heroes</span>, <span className="highlightBlue">Mobile Suit Gundam</span>, and <span className="highlightBlue">Fire Emblem</span> all serve as strong references during the creative process for this game. From the setting to the characters to the gameplay mechanics, many aspects of fantastic works were considered during the development of this project.
--- webPage/afaa-webpage/src/pages/Progress.jsx
+++ webPage/afaa-webpage/src/pages/Progress.jsx
@@ -4,12 +4,12 @@ function Progress(){
     return (
     <div>
         <div className="progressBox">
-        <h1 className="progressTitle">Progress</h1>
+        <h1 className="progressTitle">Progress & Development</h1>
         </div>
         <div className="pBodyBox">
             <p className="pBodyText"><span className="nameHighlight">A Fleet Among Angels</span> is actively under development. The game is still in the <span className="boldText">early stages</span> of development. 
             <br/><br/><br/> While there is <span className="boldText">no current progress</span> to show, here is what we are working on:
-		    <br/><br/>- Establishing core gameplay mechanics (Movement, attacks, abilities)
+		    <br/><br/>- Establishing core gameplay mechanics (Movement, Attacks, Abilities, Etc.)
             <br/><br/>- Designing and implementing the initial set of units and their skills
             <br/><br/>- Developing the first playable levels and enemy AI to publish our demo.
             <br/><br/>- Crafting the art style and user interface.
